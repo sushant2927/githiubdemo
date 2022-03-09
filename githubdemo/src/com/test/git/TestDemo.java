5e2ee3c83579ed3bdea426e9b61364fb0d15b493
asus <asus@LAPTOP-30LD131K>
2022-03-09 19:30:20 +0530

    finished push ops

--- githubdemo/src/com/test/git/TestDemo.java
+++ githubdemo/src/com/test/git/TestDemo.java
@@ -2,8 +2,8 @@ package com.test.git;
 
 public class TestDemo {
 
-	
 	public static void main(String[] args) {
 		System.out.println("First git prog");
+		System.out.println("Finished pus ops");
 	}
 }
