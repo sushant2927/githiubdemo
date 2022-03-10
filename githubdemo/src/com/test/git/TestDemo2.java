7d05d05129213a827f85e3e08e798f70c52d4c47
asus <asus@LAPTOP-30LD131K>
2022-03-10 20:49:03 +0530

    add the "." as asked by sushant2927

--- githubdemo/src/com/test/git/TestDemo2.java
+++ githubdemo/src/com/test/git/TestDemo2.java
@@ -4,10 +4,10 @@ public class TestDemo2 {
 
 	static {
 
-		System.out.println("We will be the first one");
+		System.out.println("We will be the first one.");
 	}
 
 	public static void main(String[] args) {
-		System.out.println("We are the main method");
+		System.out.println("We are the main method.");
 	}
 }
