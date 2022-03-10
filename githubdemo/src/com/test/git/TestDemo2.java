6719a87a98df002b0e60a6509781a564919e67a3
asus <asus@LAPTOP-30LD131K>
2022-03-10 20:16:59 +0530

    code checkin from codeXstream

---
+++ githubdemo/src/com/test/git/TestDemo2.java
@@ -0,0 +1,13 @@
+package com.test.git;
+
+public class TestDemo2 {
+
+	static {
+
+		System.out.println("We will be the first one");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("We are the main method");
+	}
+}
